4c4d9183f7be6ecb8cc3fdc6a524ccc06df7cc7c
ViolettaRa <78824349+ViolettaRavotto@users.noreply.github.com>
2021-05-25 12:41:58 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+function softSkills() {
+  var x = document.getElementById("mySoftSkills");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function hardSkills() {
+  var x = document.getElementById("myHardSkills");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
